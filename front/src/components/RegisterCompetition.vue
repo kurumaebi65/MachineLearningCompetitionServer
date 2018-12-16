222003c4c212f90dc2dc95a7d5d840735168d2d0
kurumaebi <ka.pay386@gmail.com>
2018-12-17 00:58:37 +0900

    create competition register page

--- front/src/components/RegisterCompetition.vue
+++ front/src/components/RegisterCompetition.vue
@@ -18,22 +18,24 @@
       <tr>
         <td>training file</td>
         <td>
-          <input type="file" id="train" accept=".csv" @change="updateSelectedFile">
+          <input type="file" id="train" accept=".csv" @change="updateTrainingFile">
         </td>
       </tr>
       <tr>
-          <td>test file</td>
+        <td>test file</td>
         <td>
-          <input type="file" id="test" accept=".csv" @change="updateSelectedFile">
+          <input type="file" id="test" accept=".csv" @change="updateTestFile">
         </td>
       </tr>
     </table>
     <input type="button" value="submit" @click="submit">
+    {{error_message}}
   </div>
 </template>
 
 <script>
 import marked from "marked";
+import axios from "axios";
 
 marked.setOptions({
   sanitize: true
@@ -44,7 +46,10 @@ export default {
     return {
       competition_name: "",
       competition_description: "",
-      competition_description_preview: ""
+      competition_description_preview: "",
+      training_file: null,
+      test_file: null,
+      error_message: ""
     };
   },
   methods: {
@@ -52,6 +57,50 @@ export default {
       this.competition_description_preview = marked(
         this.competition_description
       );
+    },
+    updateTrainingFile: function(e) {
+      e.preventDefault();
+      let files = e.target.files;
+      this.training_file = files[0];
+    },
+    updateTestFile: function(e) {
+      e.preventDefault();
+      let files = e.target.files;
+      this.test_file = files[0];
+    },
+    submit: function() {
+      if (this.training_file === null || this.test_file === null) {
+        this.error_message = "please upload file";
+        return;
+      }
+      if (this.competition_description == "") {
+        this.error_message = "please input competition description";
+        return;
+      }
+      if (this.competition_name == "") {
+        this.error_message = "please input competition name";
+        return;
+      }
+      let formData = new FormData();
+      formData.append("training_file", this.training_file);
+      formData.append("test_file",this.test_file);
+      formData.append("competition_name",this.competition_name);
+      formData.append("competition_description", this.competition_description)
+      let config = {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      };
+      axios
+        .post("api/v1/action/register_competition/", formData)
+        .then(response => {
+          this.message = response.data.message;
+          this.selectedFile = null;
+        })
+        .catch(err => {
+          this.message = err.response.data.message;
+        });
+      return;
     }
   }
 };
